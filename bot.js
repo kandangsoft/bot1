c72c85394ea7905801fc57d274eb68dde0f16f6c
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-07-24 19:15:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+/*
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -11,15 +12,14 @@ client.on('message', msg => {
         var msgt = msg.content.toLowerCase();
         var msgl = msgt.split(" ");
         var t1 = "";
-	    var t2 = "";
-        /*======================================================================*/
+	var t2 = "";
+        
 	if (msgt.includes("hamster")) {
 		switch(Math.floor((Math.random() * 2) + 1)) {
 			case 1: t2 = "https://i.ibb.co/8g9CWWy/hamster6.jpg"; break;
 			case 2: t2 = "https://i.ibb.co/H29Dkb7/hamster7.jpg"; break;
 		}
 	}
-	   /*
 	if (msgl.includes("edf")) {
 		switch(Math.floor((Math.random() * 8) + 1)) {
 			case 1: t1 = "\nTo save our mother Earth from any alien attack\nFrom vicious giant insects who have once again come back\nWe'll unleash all our forces, we won't cut them any slack\nThe EDF deploys!"; break;
@@ -32,69 +32,54 @@ client.on('message', msg => {
 			case 8: t1 = "\nA legendary hero soon will lead us to glory\nEight years ago he sunk the mothership says history\nTomorrow we will follow this brave soul to victory\nThe EDF deploys!"; break;
 		}
 	}
-	*/
-        /*
         if (msgl.includes('gay') && msgl.includes('hamster')) {
-			t1 = 'maneh nu gay';
+		t1 = 'maneh nu gay';
+	}
+	else if (msgl.includes('gandeng') && msgl.includes('hamster')) {
+		t2 = 'https://i.ibb.co/7VBx3R2/hamster2.jpg';
+	}
+	else if (msgt === 'gandeng') {
+		t2 = 'https://i.ibb.co/7VBx3R2/hamster2.jpg';
+	}
+	else if (msgl.includes("gay")) {
+		t2 = "https://i.ibb.co/q56yQKj/1244827899324.jpg";
+	}
+	else if (msgt.includes("hamster")) {
+		switch(Math.floor((Math.random() * 5) + 1)) {
+			case 1: t1 = 'uing euy...?'; break;
+			case 2: t1 = 'okaaay~'; break;
+			case 3: t1 = 'oh oh oh'; break;
+			case 4: t1 = 'aing hamster...'; break;
+			case 5: t1 = 'I love man'; break;
 		}
-		else if (msgl.includes('gandeng') && msgl.includes('hamster')) {
-			t2 = 'https://i.ibb.co/7VBx3R2/hamster2.jpg';
+
+	}
+	else if (msgl.includes("edf")) {
+		switch(Math.floor((Math.random() * 3) + 1)) {
+			case 1: t1 = 'EDF!'; break;
+			case 2: t1 = 'EDF! EDF!'; break;
+			case 3: t1 = 'EDF!! EDF!!'; break;
 		}
-		else if (msgt === 'gandeng') {
-			t2 = 'https://i.ibb.co/7VBx3R2/hamster2.jpg';
+	}
+	else if (msgl.includes("balap") || msgl.includes("meow") || msgl.includes("sonic") || msgl.includes("ucing") || msgl.includes("racing")) {
+		t1 = 'uh...meow?'
+	}
+	else if (msgl[0] == "!play") {
+		t1 = 'play weh sorangan make nitah batur sagala dasar gay!';
+	}
+	else if (msgl.includes("astolfo")) {
+		t2 = "https://i.ibb.co/XJVDqzM/astolfo.jpg";
+	}
+	else if (msgl.includes("azis")) {
+		t2 = "https://i.ibb.co/GPFY4SC/download.jpg";
+	}
+	else if (msg.attachments.size > 0) {
+		if (msg.attachments.every(attachIsImage)){
+			t2 = "https://i.ibb.co/1TZqVwT/hamster1.png";
+		} else if (msg.attachments.every(attachIsImage2)){
+			t2 = "https://i.ibb.co/1TZqVwT/hamster1.png";
 		}
-		else if (msgl.includes("gay")) {
-			t2 = "https://i.ibb.co/q56yQKj/1244827899324.jpg";
-		}
-		else if (msgt.includes("hamster")) {
-			switch(Math.floor((Math.random() * 3) + 1)) {
-				case 1: t2 = "https://i.ibb.co/k2FZRCX/hamster3.jpg"; break;
-				case 2: t2 = "https://i.ibb.co/X4hvffB/hamster4.jpg"; break;
-				case 3: t2 = "https://i.ibb.co/VpyFH09/hamster5.jpg"; break;	
-			}
-			
-			switch(Math.floor((Math.random() * 5) + 1)) {
-				case 1: t1 = 'uing euy...?'; break;
-				case 2: t1 = 'okaaay~'; break;
-				case 3: t1 = 'oh oh oh'; break;
-				case 4: t1 = 'aing hamster...'; break;
-				case 5: t1 = 'I love man'; break;
-			}
-			
-		}
-		*/
-	    /*
-		else if (msgl.includes("edf")) {
-			switch(Math.floor((Math.random() * 3) + 1)) {
-				case 1: t1 = 'EDF!'; break;
-				case 2: t1 = 'EDF! EDF!'; break;
-				case 3: t1 = 'EDF!! EDF!!'; break;
-			}
-		}
-		else if (msgl.includes("balap") || msgl.includes("meow") || msgl.includes("sonic") || msgl.includes("ucing") || msgl.includes("racing")) {
-			t1 = 'uh...meow?'
-		}
-		else if (msgl[0] == "!play") {
-			t1 = 'play weh sorangan make nitah batur sagala dasar gay!';
-		}
-		else if (msgl.includes("astolfo")) {
-			t2 = "https://i.ibb.co/XJVDqzM/astolfo.jpg";
-		}
-		else if (msgl.includes("azis")) {
-			t2 = "https://i.ibb.co/GPFY4SC/download.jpg";
-		}
-		*/
-	    /*
-	    	else if (msg.attachments.size > 0) {
-			if (msg.attachments.every(attachIsImage)){
-				t2 = "https://i.ibb.co/1TZqVwT/hamster1.png";
-			} else if (msg.attachments.every(attachIsImage2)){
-				t2 = "https://i.ibb.co/1TZqVwT/hamster1.png";
-			}
-		}
-		*/
-        
-        /*======================================================================*/
+	}
         
         if (t2 != ""){
             msg.reply("", {files: [t2]});
@@ -113,13 +98,30 @@ client.on('guildMemberAdd', member => {
 function attachIsImage(msgAttach) {
     var url = msgAttach.url;
     //True if this url is a png image.
-    return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
+    return url.indexOf("png", url.length - "png".length ) !== -1;
 }
 
 function attachIsImage2(msgAttach) {
     var url = msgAttach.url;
     //True if this url is a jpg image.
-    return url.indexOf("jpg", url.length - "jpg".length /*or 3*/) !== -1;
+    return url.indexOf("jpg", url.length - "jpg".length ) !== -1;
 }
 
 client.login(process.env.BOT_TOKEN);
+*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN).then(() => {
+    console.log("I am ready");
+    var guild = client.guilds.get('406354502023774208');
+    if(guild && guild.channels.get('406354502464045069')){
+        guild.channels.get('406354502464045069').send("Oh my body and soul...").then(() => client.destroy());
+    } else {
+        console.log("nope");
+        //if the bot doesn't have guild with the id guildid
+        // or if the guild doesn't have the channel with id channelid
+    }
+    client.destroy();
+});
