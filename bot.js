6ccad290a67ef665dcb292ad03b37f921b353512
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-07-24 21:20:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    var guild = client.guilds.get('406354502023774208');
-    if(guild && guild.channels.get('592019399062454302')){
-        guild.channels.get('592019399062454302').send("ngilu euy...").then(() => client.destroy());
+    var guild2 = client.guilds.get('406354502023774208');
+    if(guild2 && guild2.channels.get('592019399062454302')){
+        guild2.channels.get('592019399062454302').send("ngilu euy...").then(() => client.destroy());
     }
 });
 
