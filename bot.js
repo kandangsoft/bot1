a196ad6c7e03a5e1cd173e066325faad5f92da63
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-08-01 18:02:09 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     	console.log('I am ready!');
-	
+	/*
 	var guild = client.guilds.get('406354502023774208');
 	if(guild && guild.channels.get('406354502464045067')){
 		guild.channels.get('406354502464045067').send("Why don`t you touch my hamster cause it`s so sentimental night oh oh oh").then(() => client.destroy());
 	}
-	
+	*/
 });
 
 /*
