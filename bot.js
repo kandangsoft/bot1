e552b6b4e1a250419dd3de5c764c8c98223ee079
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-09-27 20:51:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on('ready', () => {
 	} else if (client.user.id == "600608154396655616") {
 		
 		var guild = client.guilds.get("406354502023774208");
-		if(guild && guild.channels.get("406354502464045067")){
-			//guild.channels.get("406354502464045067").send("Ngulub ucing di leuweung oh oh oh");
+		if(guild && guild.channels.get("592019399062454302")){
+			guild.channels.get("592019399062454302").send("balap njeng");
 		}
 	}
 });
