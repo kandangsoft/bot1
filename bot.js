e8f1535f14123fdca8bb76c4cc5e768c1d9dc013
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-11-20 20:53:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,33 +2,27 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-function testtime(arg) {
-	
-	var test_13 = "toki wa kita";
-	
-	var gld = 1;
-	
-	if (gld == 0){
-		var guild = client.guilds.get("406354502023774208");
-		if(guild && guild.channels.get("406354502464045067")){
-			guild.channels.get("406354502464045067").send(test_13);
-		}
-	} else {
-		var guild = client.guilds.get("637314014187356177");
-		if(guild && guild.channels.get("637314015214829569")){
-			guild.channels.get("637314015214829569").send(test_13);
-		}
-	}
-	
-	var jam = Math.floor((Math.random() * 3) + 5) * 10000000;
-	setTimeout(testtime, jam, 0);
-}
-
 client.on('ready', () => {
     console.log('I am ready!');
 	client.user.setActivity("");
 	
-	setTimeout(testtime, 1, 0);
-});
+	var bo_gen = "toki wa kita";
+	var bo_meow = "";
+	
+	if (bo_gen != "")
+	{
+		var guild = client.guilds.get("406354502023774208");
+		if(guild && guild.channels.get("406354502464045067")){
+			guild.channels.get("406354502464045067").send(bo_gen);
+		}
+	}
+	else if (bo_meow != "")
+	{
+		var guild = client.guilds.get("406354502023774208");
+		if(guild && guild.channels.get("592019399062454302")){
+			guild.channels.get("592019399062454302").send(bo_meow);
+		}
+	}
+});;
 
 client.login(process.env.BOT_TOKEN3);
