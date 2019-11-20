35a94b2a7418d3c64d81c814fc004ca685aef641
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-11-20 20:51:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,19 @@ const client = new Discord.Client();
 function testtime(arg) {
 	
 	var test_13 = "toki wa kita";
-	var ran = Math.floor((Math.random() * 2) + 1);
 	
-	var guild = client.guilds.get("406354502023774208");
-	if(guild && guild.channels.get("406354502464045067")){
-		guild.channels.get("406354502464045067").send(test_13);
+	var gld = 1;
+	
+	if (gld == 0){
+		var guild = client.guilds.get("406354502023774208");
+		if(guild && guild.channels.get("406354502464045067")){
+			guild.channels.get("406354502464045067").send(test_13);
+		}
+	} else {
+		var guild = client.guilds.get("637314014187356177");
+		if(guild && guild.channels.get("637314015214829569")){
+			guild.channels.get("637314015214829569").send(test_13);
+		}
 	}
 	
 	var jam = Math.floor((Math.random() * 3) + 5) * 10000000;
@@ -20,7 +28,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 	client.user.setActivity("");
 	
-	setTimeout(testtime, 1000, 0);
+	setTimeout(testtime, 1, 0);
 });
 
 client.login(process.env.BOT_TOKEN3);
