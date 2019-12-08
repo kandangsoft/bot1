5ec13a1e700c866b930012d1406790b9224866c6
kowxiii <52966616+kowxiii@users.noreply.github.com>
2019-12-09 04:55:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function testtime(arg) {
 	
 	var guild = client.guilds.get("406354502023774208");
 	if(guild && guild.channels.get("406354502464045067")){
-		guild.channels.get("406354502464045067").send("bullshit wa kita");
+		guild.channels.get("406354502464045067").send("toki wa kita");
 	}
 	
 	setTimeout(testtime, gettime(), 0);
@@ -19,7 +19,7 @@ client.on('ready', () => {
 	
 	console.log(`Logged in as ${client.user.tag}!`)
 	
-	setTimeout(testtime, 100, 0);
+	setTimeout(testtime, gettime(), 0);
 })
 
 client.login(process.env.BOT_TOKEN3);
