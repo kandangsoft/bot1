127f479f6d5f4a0ab58e9d0e182fe7e1a8c2b12f
kowxiii <52966616+kowxiii@users.noreply.github.com>
2021-03-30 17:14:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,14 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('message', msg => {
+	
+	if (msg.member.id == "826391131918041109") return;
+	
 	var msgt = msg.content.toLowerCase();
 	var t1 = "";
 	
-	if (msgt.includes("test123")) {
-		t1 = '';
+	if (msgt.includes("test12")) {
+		t1 = 'oh';
 	}
 	
 	if (t1 != "") {
